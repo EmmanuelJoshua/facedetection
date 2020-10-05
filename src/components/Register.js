b3dea766540072d42bc10fa089c1784861736cf5
Emmanuel Joshua <emmanueljoshua145@gmail.com>
2020-10-05 01:13:15 +0100

    Register, Signup

---
+++ src/components/Register.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// import {Container} from 'react-bootstrap'
+
+const Register = () => {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Register;
---
+++ src/components/SignIn.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// import {Container} from 'react-bootstrap'
+
+const SignIn = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default SignIn;
